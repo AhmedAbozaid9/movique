4db08eba775f6d5095d7f39440812cac6b6961fb
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2022-11-07 21:10:25 +0200

    fixes some bugs in review card and behave differently if there are no reviews

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,4 +1,3 @@
-import React from "react";
 import ReviewCard from "./ReviewCard";
 
 import styles from "../style/components/reviews.module.css";
@@ -7,8 +6,13 @@ function Reviews({ reviews }) {
   return (
     <section className={styles.reviews}>
       <h2 className={styles.header}>Reviews :</h2>
-      {reviews &&
-        reviews.map((review) => <ReviewCard review={review} key={review.id} />)}
+      {reviews?.length ? (
+        reviews.map((review) => {
+          return <ReviewCard review={review} key={review.id} />;
+        })
+      ) : (
+        <p>There are no reviews</p>
+      )}
     </section>
   );
 }
