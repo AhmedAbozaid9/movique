04bff2f22379c65d5d6a7b90dd6c3ac42e77fa87
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2022-11-29 17:51:12 +0200

    fixes item page not loading anything

--- src/pages/Item.js
+++ src/pages/Item.js
@@ -10,8 +10,9 @@ function Item() {
   const { state } = useLocation();
   const { itemData, itemType } = state;
   const type = itemData["media_type"] || itemType;
+  console.log(type);
   const { title, image, year } = extractData(itemData, type);
-  const genres = extractGenre(itemData["genre_ids"]);
+  const genres = extractGenre(itemData["genre_ids"], type);
 
   const reviews = useReviews(type, itemData.id);
 
